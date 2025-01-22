7661e2428be19d7daa8b75c1dadef87824ac7fec
debjit1702 <124700378+debjit1702@users.noreply.github.com>
2025-01-22 20:39:23 +0530

    integrate Replicate API for interior design image generation

--- app/api/redesign-room/route.jsx
+++ app/api/redesign-room/route.jsx
@@ -1,20 +1,33 @@
 import { NextResponse } from "next/server";
+import Replicate from "replicate";
 
-export async function POST(req){
+const replicate = new Replicate({
+    auth: process.env.NEXT_PUBLIC_REPLICATE_API_TOKEN,
+});
 
-    const { imageUrl, roomType, designType, additionalReq } =await req.json();
+export async function POST(req) {
+    const { imageUrl, roomType, designType, additionalReq } = await req.json();
 
-    //Convert the image to Ai Image
-    
+    try {
+        const input = {
+            image: imageUrl,
+            prompt: 'A' + roomType + ' with a' + designType + "style interior" + additionalReq,
+        };
 
-    //convert the output Url to BASE64 Image
+        const output = await replicate.run(
+            "adirik/interior-design:76604baddc85b1b4616e1c6475eca080da339c8875bd4996705440484a6eac38",
+            { input }
+        );
 
+        console.log(output);
+        return NextResponse.json({ result: output });
 
-    //Save Base64 to Appwrite Storage
+        // Convert the output URL to BASE64 Image
+        // Save Base64 to Appwrite Storage
+        // Save all to Database
 
-
-    // Save all to Database
-
-
-    return NextResponse.json({result: 'Hello'});
+    } catch (e) {
+        console.error(e);
+        return NextResponse.json({ error: "Something went wrong" }, { status: 500 });
+    }
 }
