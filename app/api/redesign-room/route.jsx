f2e92a3d2cf24ea36d43f19547acabe1370f4258
debjit1702 <124700378+debjit1702@users.noreply.github.com>
2025-02-25 17:48:17 +0530

    refactor: comment out Replicate API call and add placeholder output URL

--- app/api/redesign-room/route.jsx
+++ app/api/redesign-room/route.jsx
@@ -14,16 +14,20 @@ export async function POST(req) {
             prompt: 'A' + roomType + ' with a' + designType + "style interior" + additionalReq,
         };
 
-        const output = await replicate.run(
-            "adirik/interior-design:76604baddc85b1b4616e1c6475eca080da339c8875bd4996705440484a6eac38",
-            { input }
-        );
+        //const output = await replicate.run(
+       //     "adirik/interior-design:76604baddc85b1b4616e1c6475eca080da339c8875bd4996705440484a6eac38",
+       //     { input }
+       // );
 
-        console.log(output);
-        return NextResponse.json({ result: output });
+       // console.log(output);
+       // return NextResponse.json({ result: output });
+
+       const output="https://replicate.com/p/5tqds749vsrma0cmndytv248rm.png"
 
         // Convert the output URL to BASE64 Image
+
         // Save Base64 to Appwrite Storage
+        
         // Save all to Database
 
     } catch (e) {
